161aaa9186695b0e3b37c92b69f9d7e1a2a9e58b
elisabeth <elisabeth@Chiot.local>
2015-09-21 18:09:41 +0200

    Update WP

--- Wordpress/functions.php
+++ Wordpress/functions.php
@@ -242,64 +242,6 @@ remove_filter('get_the_excerpt', 'wp_trim_excerpt');
 add_filter('get_the_excerpt', 'custom_wp_trim_excerpt'); 
 
 /*-----------------------------------------------------------------------------------*/
-/* Markup gallery
-/*-----------------------------------------------------------------------------------*/
-function my_post_gallery( $output, $attr) {
-    global $post, $wp_locale;
-    static $instance = 0;
-    $instance++;
-
-    if( isset($attr['orderby']) ){
-        $attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
-        if( !$attr['orderby'] ) unset( $attr['orderby'] );
-    }
-
-    extract(shortcode_atts(array(
-        'order'      => 'ASC',
-        'orderby'    => 'menu_order ID',
-        'id'         => $post->ID,
-        'itemtag'    => '',
-        'icontag'    => '',
-        'captiontag' => '',
-        'columns'    => 3,
-        'size'       => 'presse-logo',
-        'include'    => '',
-        'exclude'    => ''
-    ), $attr));
-
-    $id = intval($id);
-    if( 'RAND' == $order ) $orderby = 'none';
-
-    if( !empty($include) ){
-        $include = preg_replace( '/[^0-9,]+/', '', $include );
-        $_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
-
-        $attachments = array();
-        foreach( $_attachments as $key => $val ){
-            $attachments[$val->ID] = $_attachments[$key];
-        }
-    }elseif ( !empty($exclude) ){
-        $exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
-        $attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
-    }else{
-        $attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
-    }
-
-    if( empty($attachments) ) return '';
-
-    $selector = "gallery-{$instance}";
-    $output = "<li id='$selector'>";
-
-    foreach( $attachments as $id => $attachment ){
-        $output .= '<div>' . wp_get_attachment_image($id, $size) . '</div>';
-    }
-
-    $output .= "</li>";
-    return $output;
-}
-add_filter( 'post_gallery', 'my_post_gallery', 10, 2 );
-
-/*-----------------------------------------------------------------------------------*/
 /* Remove default WYSIWYG editor in Custom
 /*-----------------------------------------------------------------------------------*/
 function hide_editor() {
